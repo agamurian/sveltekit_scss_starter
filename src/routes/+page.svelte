1e43e08c412e6caf79c35e08532277469f390292
agamurian <agamurian@gmail.com>
2026-07-07 19:56:29 +0200

    refine typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,24 @@
 <section class="container-centered">
-<h2>Welcome to SvelteKit</h2>
-<h3>
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</h3>
-
-
+<h1>How this website resolves typography</h1>
+<h6>
+Short intruduction to typography
+</h6>
+<hr/>
 <div class="blocks">
 <div class="block">
 <h4>
-documentation:
+Rule of thumb: it looks good
 </h4>
-
 <p>
-
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation 
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-
+Counterintuitevely, this is not what you would thought in this kind of situaltion
 </p>
-
 </div>
 <div class="card">
-
-<h4>
-documentation:
-</h4>
-
+<h5>
+Sidenote:
+</h5>
 <p>
-
-Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation 
 Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development. It is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words are the truncation of dolorem ipsum. Lorem ipsum's purpose is to permit a page layout to be designed, independently of the copy that will subsequently populate it, or to demonstrate various fonts of a typeface without meaningful text that could be distracting
-
 </p>
 </div>
 <div class="block">
@@ -43,9 +26,7 @@ Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publ
 <h4>
 documentation:
 </h4>
-
 <p>
-
 Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p><p>
 Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development. It is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words are the truncation of dolorem ipsum. Lorem ipsum's purpose is to permit a page layout to be designed, independently of the copy that will subsequently populate it, or to demonstrate various fonts of a typeface without meaningful text that could be distracting
 Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
@@ -95,6 +76,7 @@ Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the 
 <h4>
 documentation:
 </h4>
+<hr>
 
 <p>
 
