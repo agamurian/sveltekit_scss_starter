26271332c8f114e8325e2e9e674cdb38e4275b3b
agamurian <agamurian@gmail.com>
2026-07-11 22:08:45 +0200

    clear

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,6 @@ Counterintuitevely, this is not what you would thought in this kind of situaltio
 <h5>
 Sidenote:
 </h5>
-<hr/>
 <p>
 Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development. It is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words are the truncation of dolorem ipsum. Lorem ipsum's purpose is to permit a page layout to be designed, independently of the copy that will subsequently populate it, or to demonstrate various fonts of a typeface without meaningful text that could be distracting
 </p>
